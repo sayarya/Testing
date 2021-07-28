8c9e78941794aa50ec401c58678453cdc198edee
sayarya <64267777+sayarya@users.noreply.github.com>
2021-07-28 11:19:19 +0530

    Create DummyEmailServiceImpl.java

---
+++ src/test/java/com/example/demo/dummy/DummyEmailServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.dummy;
+
+public class DummyEmailServiceImpl implements EmailService {
+
+	@Override
+	public void sendMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
